4f6c9ff1adea95d5c21aa3e902c32059a69299f3
Syed Shahzaib <75627602+SyedShahzaibssuet@users.noreply.github.com>
2021-06-06 15:37:57 -0700

    Update homepage.dart

--- lib/src/screen/homepage.dart
+++ lib/src/screen/homepage.dart
@@ -12,57 +12,66 @@ class _Homestate extends State<Homepage> {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          elevation: 0,
-          title: Text("Home"),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {},
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: Text("Home"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => History()));
+            },
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => History()));
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.account_balance_outlined),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Account()));
-              },
-            ),
-            SizedBox(width: 10.0),
-          ],
+          IconButton(
+            icon: Icon(Icons.account_balance_outlined),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Account()));
+            },
           ),
-      body: Column(
-        children: [
+          SizedBox(width: 10.0),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
           Category(),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           item(0, 1),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           item(2, 3),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
           item(4, 5),
         ]),
+      ),
     );
   }
 
   Row item(index1, index2) {
-    return Row(children: [
+    return Row(
+      mainAxisAlignment:MainAxisAlignment.center,
+      children: [
+      // SizedBox(width: 20),
       itemcard(index1),
-      SizedBox(width:20),
+      SizedBox(width: 20),
       itemcard(index2),
     ]);
   }
 
   Column itemcard(index) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(width:20),
+        SizedBox(width: 20),
         Container(
           padding: EdgeInsets.all(20.0),
           height: 200,
@@ -71,10 +80,18 @@ class _Homestate extends State<Homepage> {
             color: products[index].color,
             borderRadius: BorderRadius.circular(18),
           ),
-          child: Image.asset(products[index].image),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Image.asset(products[index].image),
+              SizedBox(height: 20),
+              Center(
+                  child: Text(products[index].title,
+                      style: TextStyle(fontWeight: FontWeight.bold))),
+              Center(child: Text("${products[index].price}")),
+            ],
+          ),
         ),
-        Text(products[index].title),
-        Text("${products[index].price}"),
       ],
     );
   }
